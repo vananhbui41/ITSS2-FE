4458f1a56c5df50c27a155ef084d4a7cadb2809d
nguyenhuongvi1712 <huongvinguyen171200@gmail.com>
2022-12-20 15:27:17 +0700

    fix search

--- src/api/search/index.js
+++ src/api/search/index.js
@@ -21,7 +21,7 @@ export async function search({ keyword, type, context, topic }) {
           keyword,
           tags,
         },
-        config,
+        ...config,
       });
       return {
         success: true,
@@ -30,7 +30,7 @@ export async function search({ keyword, type, context, topic }) {
     }
     const res = await axiosInstance.get('search', {
       params: { tags },
-      config,
+      ...config,
     });
     return {
       success: true,
